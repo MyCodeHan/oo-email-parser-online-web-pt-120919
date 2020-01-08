5c5af03a9ff18bdb916a44fc279ae3e441a60b79
Hannah Harrington <hannahharrington@outlook.com>
2020-01-08 04:09:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,6 +15,6 @@ class EmailParser
     # select from the array only those emails that are not blank spaces
     # select only those unique emails
     emails.split(/[,?\s]/).select{|email| email != ""}.uniq 
-
+  end
 end
 
